796a92ff393c5215df533228b8e1f52ec440f13a
Mukhammed <zhumalimuhammed@gmail.com>
2025-12-30 15:36:45 +0500

    little edit

--- src/main/java/backend/model/auth/LoginRequest.java
+++ src/main/java/backend/model/auth/LoginRequest.java
@@ -5,10 +5,7 @@ import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
@@ -16,7 +13,6 @@ import lombok.Setter;
 public class LoginRequest {
     @NotBlank(message = "Username is required")
     private String username;
-
     @NotBlank(message = "Password is required")
     private String password;
 }
